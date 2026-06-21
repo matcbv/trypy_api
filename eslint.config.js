ea28b097034046684c57e10de4cd41bc37a0ac53
matcbv <matheuscbv23@gmail.com>
2026-06-21 12:32:29 -0300

    Ajustes no arquivo de configuracao do eslint com adicao do pacote de configuracao do prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,43 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
 
 // Utilizando flat config (Forma mais moderna de trabalhar com Eslint):
-export default [
+export default defineConfig([
 	pluginJs.configs.recommended,
 	{
 		// Arquivos onde o Eslint deve ser aplicado
-		files: ["src/**/*.js"],
+		files: ['src/**/*.js'],
 		// Propriedade da linguagem sendo utilizada
 		languageOptions: {
 			// Define o tipo de variáveis globais que estarão disponíveis no ambiente de programação
 			globals: globals.node,
-			sourceType: "script",
-			ecmaVersion: 2022,
-		},
-	},
-	{
-		files: ["frontend/js/*.js"],
-		languageOptions: {
-			globals: globals.browser,
-			sourceType: "module",
+			sourceType: 'module',
 			ecmaVersion: 2022,
 		},
 	},
 	{
 		rules: {
 			// Garante que sejam usadas apenas operadores de comparação estrita.
-			eqeqeq: ["error"],
-			// Garante o uso de chaves em estruturas condicionais e de controle de fluxo. Com all, garantimos que a regra seja estrita para todos os casos.
-			curly: ["error", "all"],
+			eqeqeq: ['error'],
 			// Define o número máximo de linhas vazias entre códigos.
-			"no-multiple-empty-lines": ["error", { max: 2 }],
+			'no-multiple-empty-lines': ['error', { max: 2 }],
 			// Garante o uso de Camel Case em nosso código.
-			camelcase: ["error"],
+			camelcase: ['error'],
 			// Regra para não uso de mensagem de alerta em geral.
-			"no-alert": ["warn"],
+			'no-alert': ['warn'],
+			// Regra para alertar sobre o não uso de alguma variável.
+			'no-unused-vars': ['error'],
 		},
 	},
 	{
 		// Arquivos a serem ignorados (node_modules é ignorado por padrão):
-		ignores: ["public/js/**"],
+		ignores: [],
 	},
 	eslintConfigPrettier,
-];
+	{
+		// Garante o uso de chaves em estruturas condicionais e de controle de fluxo. Com multi-line, garantimos que a regra seja aplicada a casos onde a estrutura condicional ocupe mais de uma linha.
+		curly: ['error', 'multi-line'],
+	},
+]);
